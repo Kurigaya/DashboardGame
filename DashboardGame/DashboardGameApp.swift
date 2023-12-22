83e60258e72a2273a6f98a414c1255779dbfa2ff
Bright_Peerapat <Peerapat@example.com>
2023-12-22 13:44:59 +0700

    Initial Commit

---
+++ DashboardGame/DashboardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardGameApp.swift
+//  DashboardGame
+//
+//  Created by Mac-Peerapat on 22/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DashboardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
